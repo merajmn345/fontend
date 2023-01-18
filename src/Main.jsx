a056e2e4697427f0350a585a53fa74434c5595d6
merajmn345 <98653773+merajmn345@users.noreply.github.com>
2023-01-18 16:50:46 +0530

    Initial commit

---
+++ src/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Main() {
+  return (
+    <header className="note">
+      <h1 className="h1">DevTown</h1>
+      <p>
+        We at DevTown (CODETOWN EDTECH PRIVATE LIMITED) (A research and
+        education-based startup ), are a team with an aim to provide students
+        with a platform to research and make projects in the field of AI and ML.{" "}
+      </p>
+    </header>
+  );
+}
+export default Main;
